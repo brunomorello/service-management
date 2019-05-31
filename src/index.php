e77f71a06d1cea317439b806d3b5ff11034f2688
Bruno Moreno <bmorenos@br.ibm.com>
2019-05-30 21:52:09 -0300

    Initialize Repository

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<? 
+
+$test = "Hello World!"; 
+
+$db = new PDO('mysql:host=database;dbname=smanagement;charset=utf8mb4', 'root', 'docker');
+
+$databaseTest = ($db->query('SELECT * FROM first_deploy'))->fetchAll(PDO::FETCH_OBJ);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bruno Moreno - Service Management</title>
+</head>
+<body>
+    <h1><?= $test ?></h1>
+
+    <? foreach ($databaseTest as $row): ?>
+        <p>Hello, <?= $row->first_name ?></p>
+    <? endforeach; ?>
+</body>
+</html>
