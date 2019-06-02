7ed5c331d002d8ba063e3ff98e96ab171ad8f094
Bruno Moreno <brunomorello7@gmail.com>
2019-06-02 09:54:05 -0300

    MYSQL Dump example for application

---
+++ docker/database/smanagment-schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS `smanagement`;
+
+USE `smanagement`;
+
+CREATE TABLE IF NOT EXISTS `employees` (
+    `first_name` varchar(50),
+    `last_name` varchar(100),
+    `email` varchar(100)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `employees`;
+
+INSERT INTO `employees` VALUES('Bruno', 'Moreno', 'brunomorello7@gmail.com'), ('User1', 'Test', 'user1@test.com');
+
+UNLOCK TABLES;
